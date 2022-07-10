129ccd42a3c93ff734acff59dbd1c0872ff109b6
aliyukelani <kelanialiu@gmail.com>
2022-07-10 02:45:26 -0700

    /**

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
-/*
- *_putchar - putchar prototype
- *@c: input of type char to print
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- *Return: on success 1
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
