aefea42cf22ab6d5fa9c0725466083752ffa88ad
aliyukelani <kelanialiu@gmail.com>
2022-07-16 08:44:33 -0700

    10-print_triangle.c v1

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle using '#' n times
+ *@n: number of times to print # into a triangle
+ *
+ * Return: nothing
+ */
+void print_triangle(int n)
+{
+	int i, j;
+
+	i = 0;
+	do {
+		for (j = 0; j < n; j++)
+		{
+			if (j == (n - 1))
+				_putchar('#');
+			else
+				_putchar('#');
+		}
+		_putchar(10);
+		i++;
+	} while (i < n);
+}
