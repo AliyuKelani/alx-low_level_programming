5bed97dcee3384d8ee928a2cc7b76d5ed98fd58b
aliyukelani <kelanialiu@gmail.com>
2022-07-17 07:29:42 -0700

    101-print_number.c v1

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints any integer
+ *@n: integer to write
+ *
+ * Return: Nothing.
+ */
+void print_number(int n)
+{
+	int len =  0, temp, i, lead, pow;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+
+	temp = n;
+	while (temp >= 10)
+	{
+		len++;
+		temp /= 10;
+	}
+
+	pow = 1;
+	for (i = 0; i < len; i++)
+		pow *= 10;
+
+	for (i = 0; i <= len; i++)
+	{
+		lead = n / pow;
+		n = n - (lead * pow);
+		pow /= 10;
+		_putchar('0' + lead);
+	}
+}
