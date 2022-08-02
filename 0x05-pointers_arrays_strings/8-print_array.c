f270c5416bd9f265c42b4f9edcaddabeba013539
aliyukelani <kelanialiu@gmail.com>
2022-08-02 10:31:04 -0700

    8-print_array.c v2

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,15 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+	int i, size = 0;
 
-	for (i = 0; i < n; i++)
+	size = sizeof(*a) / sizeof(int);
+	for (i = 0; i < n && i < size; i++)
 	{
 		printf("%d", a[i]);
 		if ((i + 1) != n)
 			printf(",");
 	}
+	printf("%d", size);
 	printf("%c", 10);
 }
