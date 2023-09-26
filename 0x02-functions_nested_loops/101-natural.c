139e9802c9a33f35d1ef19075d000e8bdb2d5b56
kelani <kelanialiyu@gmail.com>
2023-09-26 17:07:56 +0100

    for betty

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-  int i, sum;
+	int i, sum;
 
-  sum = 0;
+	sum = 0;
 	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
-			{
-				sum += i;
-			}
+		{
+			sum += i;
+		}
 	}
-  printf("%d\n", sum);
-  return (0);
+	printf("%d\n", sum);
+	return (0);
 }
