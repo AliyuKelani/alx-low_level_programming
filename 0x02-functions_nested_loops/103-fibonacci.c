a056ea87e0ab821af716e9a14eba929fd37257d9
kelani <kelanialiyu@gmail.com>
2023-09-26 17:29:36 +0100

    fib 104

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - sum of even fibonacci numbers under 4,000,000
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	long int a, b, c;
+	long int sum = 0;
+	a = 1;
+	b = 2;
+	c = a + b;
+
+	while (c < 4000000)
+	{
+		if (c % 2 == 0)
+			sum += c;
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
